f951fdd567d1c6e4cf1aeb2f432d90d10221f0d7
anupsinghh <itsanupsingh018@gmail.com>
2025-05-03 22:38:36 +0530

    solved: maximum sum subarray

--- arrays/01_maximum_sum_subarray.cpp
+++ arrays/01_maximum_sum_subarray.cpp
@@ -1,1 +1,18 @@
-﻿// LeetCode 53: Maximum Sum Subarray
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+    int maxSum = nums[0], currSum = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+        currSum = max(nums[i], currSum + nums[i]);
+        maxSum = max(maxSum, currSum);
+    }
+    return maxSum;
+}
+
+int main() {
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << maxSubArray(nums) << endl;
+    return 0;
+}
