8d66c77d6288d2ed83434b81927f92744dba029b
anupsinghh <itsanupsingh018@gmail.com>
2025-05-05 19:50:22 +0530

    solved: kth largest element

--- arrays/02_kth_largest_element.cpp
+++ arrays/02_kth_largest_element.cpp
@@ -1,1 +1,20 @@
-﻿// LeetCode 215: Kth Largest Element
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    for (int n : nums) {
+        minHeap.push(n);
+        if (minHeap.size() > k) minHeap.pop();
+    }
+    return minHeap.top();
+}
+
+int main() {
+    vector<int> nums = {3, 2, 1, 5, 6, 4};
+    int k = 2;
+    cout << findKthLargest(nums, k) << endl;
+    return 0;
+}
