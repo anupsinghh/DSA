90847af6c83681e38338cda9e47fa9973f73475e
anupsinghh <itsanupsingh018@gmail.com>
2025-05-06 20:30:57 +0530

    solved: rotate array

--- arrays/03_rotate_array.cpp
+++ arrays/03_rotate_array.cpp
@@ -1,1 +1,19 @@
-﻿// LeetCode 189: Rotate Array
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void rotate(vector<int>& nums, int k) {
+    k %= nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    rotate(nums, k);
+    for (int n : nums) cout << n << " ";
+    return 0;
+}
