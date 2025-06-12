b04e5b9972ba7584027625f8fde9967793ebe32f
Anup Singh <itsanupsingh018@gmail.com>
2025-06-12 14:03:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (29.05%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -32,3 +32,19 @@ public:
         return dummy.next;
     }
 };
+// Recursve Approach
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if (!list1) return list2;  // If list1 is empty, return list2
+//         if (!list2) return list1;  // If list2 is empty, return list1
+        
+//         if (list1->val < list2->val) {
+//             list1->next = mergeTwoLists(list1->next, list2);
+//             return list1;
+//         } else {
+//             list2->next = mergeTwoLists(list1, list2->next);
+//             return list2;
+//         }
+//     }
+// };
