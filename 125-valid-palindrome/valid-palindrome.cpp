768f7a5095159a0197a4f07bb9c57b32f33be6b2
Anup Singh <itsanupsingh018@gmail.com>
2025-08-10 17:01:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,14 +1,14 @@
 class Solution {
-public:
-    bool isPalindrome(string s) {
-        string clean;
-        for (char c : s) {
-            if (isalnum(static_cast<unsigned char>(c)))
-                clean += tolower(static_cast<unsigned char>(c));
+    public:
+        bool isPalindrome(string s){
+            int i=0, j=s.size()-1;
+            while(i<j){
+                if(!isalnum(s[i])){i++;continue;}
+                if(!isalnum(s[j])){j--;continue;}
+                if(tolower(s[i]) != tolower(s[j])) return false;
+                i++;j--;
+            }
+            return true;
+
         }
-        int n = clean.size();
-        for (int i = 0; i < n / 2; ++i)
-            if (clean[i] != clean[n - i - 1]) return false;
-        return true;
-    }
 };
